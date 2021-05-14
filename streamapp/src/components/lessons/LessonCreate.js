067b01f943d62a2e913ead3cd8dcfca48e8f0d21
chenfrank647 <chenfrank647@gmail.com>
2021-05-14 13:56:37 -0400

    JSON

--- streamapp/src/components/lessons/LessonCreate.js
+++ streamapp/src/components/lessons/LessonCreate.js
@@ -1,10 +1,56 @@
 import React from 'react';
+import {Field, reduxForm} from 'redux-form';
 
+class LessonCreate extends React.Component {
 
-const LessonCreate = () => {
+    renderError({touched, error}){
+        if (touched && error){
+            return (
+                <div className="ui error message">
+                    <div className="header">{error}</div>
+                </div>
+            );
+        }
+    }
+
+    renderInput = (formProps) => {
+        return (
+            <div className="field">
+                <label>{formProps.label}</label>
+                <input onChange={formProps.input.onChange} value = {formProps.input.value}/>
+                {this.renderError(formProps.meta)}
+            </div>    
+        );
+    }
+    onSubmit(formValues){
+        
+    }
+
+   render(){
     return (
-        <div>LessonCreate</div>
+        <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form error">
+            <Field name = "title" component = {this.renderInput}  label = "Enter Course Name"/>
+            <Field name = "description" component = {this.renderInput} label = "Provide a description"/>
+            <Field name = "audience" component = {this.renderInput} label = "Target grades"/>
+            <button className = "ui button primary">Submit</button>
+        </form>
     );
+   }
 };
 
-export default LessonCreate;
+
+
+const validate = (formValues) => {
+    const errors = {};
+    if(!formValues.title){
+        errors.title = 'Your lesson needs a title';
+    }else if(!formValues.description){
+        errors.description = 'Please add a short description for your lesson'
+    }
+    return errors;
+}
+
+export default reduxForm({
+    form: 'lessonCreate',
+    validate: validate
+}) (LessonCreate);
